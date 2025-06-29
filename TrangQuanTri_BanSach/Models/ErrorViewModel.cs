bd2c8558d059e6bddaa104e1448d727798a1abda
ThinhPC <ThinhPC@DESKTOP-A3VEBPN>
2025-06-29 23:19:25 +0700

    Add project files.

---
+++ TrangQuanTri_BanSach/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrangQuanTri_BanSach.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
