56084d02295d6dbb83e2e9ea17cc607e0a18d665
thinh <thinhqn999@gmail.com>
2025-07-22 20:20:03 +0700

    feat(publisher): tạo service

---
+++ TrangQuanTri_BanSach/Repo/Abstract/IPublisherService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TrangQuanTri_BanSach.Models.Domain;
+
+namespace TrangQuanTri_BanSach.Repo.Abstract
+{
+  public interface IPublisherService
+  {
+    bool Add(Publisher model);
+    bool Update(Publisher model);
+    bool Delete(int id);
+    Publisher FindById(int id);
+    IEnumerable<Publisher> GetAll();
+  }
+}
---
+++ TrangQuanTri_BanSach/Repo/Implement/PublisherService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using TrangQuanTri_BanSach.Models.Domain;
+using TrangQuanTri_BanSach.Repo.Abstract;
+
+namespace TrangQuanTri_BanSach.Repo.Implement
+{
+  public class PublisherService : IPublisherService
+  {
+    private readonly DatabaseContext _dbContext;
+
+    public PublisherService(DatabaseContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
+
+    public bool Add(Publisher model)
+    {
+      try
+      {
+        _dbContext.Publisher.Add(model);
+        _dbContext.SaveChanges();
+        return true;
+      }
+      catch (System.Exception)
+      {
+        return false;
+      }
+    }
+
+    public bool Delete(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Publisher FindById(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<Publisher> GetAll()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Update(Publisher model)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
